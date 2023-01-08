a919ba5415ce33140bf754a008cd5d81636f3b5d
sadyr <sadyr0012@gmail.com>
2023-01-09 00:06:00 +0600

    Flask upd

--- flask/projectLibrary/library/book.py
+++ flask/projectLibrary/library/book.py
@@ -14,11 +14,23 @@ menus = [
         {'name': 'Фильмы', 'url':"#"},
     ]
 
+
+book_list = [
+        {'name': 'Название'},
+         {'name': 'Описание'},
+        {'name': 'Автор'},
+        {'name': 'Жанр'},
+        {'name': 'Статус'}
+    ]
+
+
+
+
 @bp.route('/books')
 def books():   # Функция для главной страницы сайта
     db = get_db()
     books = db.execute(
-       'SELECT  title, description,author_id,jenre_id, status_id, jenre.name '
+       'SELECT  books.id, title, description,author_id,jenre_id, status_id, jenre.name '
         ' FROM books  JOIN jenre  ON books.jenre_id = jenre.id'
     ).fetchall()
     title = 'Books'
@@ -68,9 +80,86 @@ def add_book():
         'SELECT * '
         ' FROM status'
     ).fetchall()
-    st = type(statuses)
 
     return render_template('book/add_book.html', authors=authors, jenres = jenres, statuses = statuses, menus=menus)
+
+
+
+@bp.route('/books/show_book/<int:book_id>')
+@login_required
+def show_book(book_id):
+    db = get_db()
+    book = db.execute(
+        'SELECT  books.id as id, title, description, authors.name as author , jenre.name as jenre, status.name as status'
+        ' FROM books'
+        ' INNER JOIN  authors ON books.author_id = authors.id'
+        ' INNER JOIN  jenre ON books.jenre_id = jenre.id'
+        ' INNER JOIN  status ON books.status_id = status.id'
+        ' WHERE books.id = ?',
+        (book_id,)
+    ).fetchone()
+    return render_template('book/show_book.html', book = book, title = 'Книга', menus=menus ,  zip=zip)
+
+@bp.route('/books/edit_book/<int:book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        author_id = request.form['author']
+        jenre_id = request.form['jenre']
+        status_id = request.form['status']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE books'
+                ' SET title = ?, description = ?,author_id = ?, jenre_id = ?, status_id = ?'
+                ' WHERE id = ?',
+                (title, description, author_id, jenre_id, status_id, book_id)
+            )
+            db.commit()
+            return redirect(url_for('book.books'))
+    db = get_db()
+    book = db.execute(
+        'SELECT  title, description, authors.name as author , jenre.name as jenre, status.name as status'
+        ' FROM books'
+        ' INNER JOIN  authors ON books.author_id = authors.id'
+        ' INNER JOIN  jenre ON books.jenre_id = jenre.id'
+        ' INNER JOIN  status ON books.status_id = status.id'
+        ' WHERE books.id = ?',
+        (book_id,)
+    ).fetchone()
+    authors = db.execute(
+    'SELECT * '
+    ' FROM authors').fetchall()
+    jenres = db.execute('SELECT * '
+    ' FROM jenre').fetchall()
+    statuses = db.execute(
+    'SELECT * '
+    ' FROM status').fetchall()
+    return render_template('book/edit_book.html', book=book, authors=authors, jenres = jenres, statuses = statuses, menus=menus)
+
+@bp.route('/books/delete_book/<int:book_id>', methods=['GET', 'POST'])
+@login_required
+def delete_book(book_id):
+    book_id = book_id
+    error = None
+    db = get_db()
+    db.execute(
+        'DELETE'
+        ' FROM books '
+        ' WHERE id = ?', 
+        (book_id,)
+    )
+    db.commit()
+    return redirect(url_for('book.books'))
+
 
 @bp.route('/add_jenre', methods=['GET', 'POST']) # Функция для добавления жанров
 @login_required
